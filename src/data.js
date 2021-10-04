aa625aa63e995f8f7130f8c6481a2cd7208d7db4
jensbols <jens.bols@student.hogent.be>
2021-10-04 11:10:49 +0200

    project component

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: "Project I",
+    subtitle: "Project I Subtitle",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Project I",
+    subtitle: "Project I Subtitle",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Project I",
+    subtitle: "Project I Subtitle",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Project I",
+    subtitle: "Project I Subtitle",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  }
+]
